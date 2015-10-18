f68d0b30bcc7060a56b6985c2a0a350830999ecf
Mikkel Kjeldsen <commonquail@gmail.com>
2015-10-18 17:11:55 +0200

    Enforce singleton property with enum
    Per Effective Java, 2nd Edition, Item 3, this commit changes the default
    singleton template to use an enum instead of a class. Doing so
    automatically solves all serialization issues. However, because 1) this
    pattern remains rare in the wild (even for singletons, which should be
    rare on their own); 2) it unavoidably clutters the interface with enum
    related methods; and 3) enum constructors are always private, the
    classic implementation is moved to another file.

--- fileTemplates/Singleton.java
+++ fileTemplates/Singleton.java
@@ -1,12 +1,8 @@
 #if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
 #parse("File Header.java")
-public final class ${NAME} {
-    private static final ${NAME} INSTANCE = new ${NAME}();
+public enum ${NAME} {
+    INSTANCE;
 
-    public static ${NAME} getInstance() {
-        return INSTANCE;
-    }
-
-    private ${NAME}() {
+    ${NAME}() {
     }
 }
